733b1dd91de27722505b9117873851634d422004
Glen Alloy <glenalloy8@gmail.com>
2024-10-30 12:34:52 +0530

    implmeneted side bar and top nav bar

--- Green-Shadow-front-end/js/script.js
+++ Green-Shadow-front-end/js/script.js
@@ -1,2 +1,12 @@
-$("#login").css({display:'block'});
-$("#dashboard-section").css({display: 'none'});
+$("#login-section").css({display:'none'});
+$("#navbar-section").css({display: 'block'});
+
+$(document).ready(function(){
+    $(".hamburger .hamburger__inner").click(function(){
+      $(".wrapper").toggleClass("active")
+    })
+
+    $(".top_navbar .fas").click(function(){
+       $(".profile_dd").toggleClass("active");
+    });
+})

a84e2034614c688373e8bed205bfdfcaa07037ce
Glen Alloy <glenalloy8@gmail.com>
2024-11-01 11:05:09 +0530

    implemented navigation of crop section in script js

--- Green-Shadow-front-end/js/script.js
+++ Green-Shadow-front-end/js/script.js
@@ -1,13 +1,22 @@
 $("#login-section").css({display:'none'});
 $("#dashboard-section").css({display: 'block'});
 $("#field-section").css({display:'none'});
+$("crop-section").css({display:'none'});
 
 $('#nav-dashboard').on('click', () => {
   $('#dashboard-section').css({display: 'block'});
   $('#field-section').css({display: 'none'});
+  $('#crop-section').css({display: 'none'});
 });
 
 $('#nav-field').on('click', () => {
-  $('#dashboard-section').css({display: 'none'});
   $('#field-section').css({display: 'block'});
+  $('#dashboard-section').css({display: 'none'});
+  $('#crop-section').css({display: 'none'});
+});
+
+$('#nav-crop').on('click', () => {
+  $('#crop-section').css({display: 'block'});
+  $('#dashboard-section').css({display: 'none'});
+  $('#field-section').css({display: 'none'});
 });

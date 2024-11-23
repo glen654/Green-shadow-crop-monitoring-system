cb8c69ef1c360edabcd64dd63f5a0bce8dc6c1e1
glen654 <glenalloy@gmail.com>
2024-11-23 21:12:15 +0530

    debugged the fuction savefield

--- Green-Shadow-front-end/controllers/FieldController.js
+++ Green-Shadow-front-end/controllers/FieldController.js
@@ -1,31 +1,32 @@
 function saveField(){
-    var field_name = $("#field_name").val();
-    var field_location_x = $("#field_location_x").val();
-    var field_location_y = $("#field_location_y").val();
-    var field_size = $("#field_save").val();
-    var field_image1 = $("#field_image1").val();
-    var field_image2 = $("#field_image2").val();
+    const formData = new FormData(); 
+    
+    formData.append("field_name", $("#field_name").val());
+    
+    const location_x = parseInt($("#field_location_x").val());
+    formData.append("x", location_x);
+
+    const location_y = parseInt($("#field_location_y").val());
+    formData.append("y", location_y);
+
+    formData.append("extent_size", $("#field_size").val());
+
+    formData.append("field_image1", $("#field_image1")[0].files[0]);
+    formData.append("field_image2", $("#field_image2")[0].files[0]);
 
     $.ajax({
-        url: " http://localhost:5050/green-shadow/api/v1/field",
-        method:"POST",
-        contentType:"multipart/form-data",
-        "data":JSON.stringify({
-            "field_name":field_name,
-            "x":field_location_x,
-            "y":field_location_y,
-            "extent_size":field_size,
-            "field_image1":field_image1,
-            "field_image2":field_image2
-        }),
-        success:function(result){
+        url:"http://localhost:5050/green-shadow/api/v1/field",
+        method: "POST",
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function (result){
             console.log(result);
-            alert("Customer saved successfully");
-
+            alert("Field Save Successfull");
         },
-        error:function(result){
-            alert("Customer save unsuccessful");
+        error: function (result){
             console.log(result);
+            alert("Field Save Unsuccessfull");
         }
-    })
+    });
 }

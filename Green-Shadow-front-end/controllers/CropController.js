90f2f97ac3339fc201c89d61bc18fc28a0ac0edc
glen654 <glenalloy@gmail.com>
2024-11-24 13:01:55 +0530

    change the port number in save crop function

--- Green-Shadow-front-end/controllers/CropController.js
+++ Green-Shadow-front-end/controllers/CropController.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
 function saveCrop(){
     const formData = new FormData();
 
-    formData.append("commonName", $("#crop_common_name").val());
-    formData.append("scientificName", $("#crop_scientific_name").val());
-    formData.append("cropImage", $("#crop_image")[0].files[0]);
+    formData.append("common_name", $("#crop_common_name").val());
+    formData.append("scientific_name", $("#crop_scientific_name").val());
+    formData.append("crop_image", $("#crop_image")[0].files[0]);
     formData.append("category", $("#category").val());
     formData.append("season", $("#crop_season").val());
-    formData.append("fieldDTO", $("field_details").val());
+    formData.append("field_name", $("field_details").val());
 
     $.ajax({
-        url:" http://localhost:8080/green-shadow/api/v1/crop",
+        url:" http://localhost:5050/green-shadow/api/v1/crop",
         method: "POST",
         contentType: false,
         processData: false,
@@ -57,7 +57,6 @@ function clearFields(){
     $("#crop_common_name").val('');
     $("#crop_scientific_name").val('');
     $("#crop_image").val('');
-    $("#category").val('');
+    $("#crop_category").val('');
     $("#crop_season").val('');
-    $("#field_details").val('');
 }

941672e7f89e9942d857584f604808af94b01369
glen654 <glenalloy@gmail.com>
2024-11-30 20:57:30 +0530

    field update function implemented on field controller

--- Green-Shadow-front-end/controllers/FieldController.js
+++ Green-Shadow-front-end/controllers/FieldController.js
@@ -39,7 +39,24 @@ function loadFields(){
             console.error("Failed to load fields:", error);
             alert("An error occurred while loading the field data.");
         }
+
     });
+
+    // $("#fields-table").on('click', '.update-button', function() {
+    //     console.log("Update button clicked");
+    //     let index = $(this).index();
+    //     recordIndex = index;
+    
+
+    //     let field_name = $(this).find(".field-name-value").text();
+    //     let field_location = $(this).find(".field-location-value").text();
+    //     let extent_size = $(this).find(".extent-size-value").text();
+
+    //     $("#field_name").val(field_name);
+    //     $("#field_location_x").val(field_location);
+    //     $("#field_location_y").val(field_location);
+    //     $("#field_size").val(extent_size);
+    // });
 }
 
 function saveField(){
@@ -68,14 +85,17 @@ function saveField(){
             clearFields();
             console.log(result);
             alert("Field Save Successfull");
+            loadFields();
         },
         error: function (result){
             clearFields();
             console.log(result);
             alert("Field Save Unsuccessfull");
+            loadFields();
         }
     });
 }
+
 
 $("#fields-table").on('click','tr',function (){
     let index = $(this).index();
@@ -94,6 +114,45 @@ $("#fields-table").on('click','tr',function (){
     $("#field_location_y").val(y);
     $("#field_size").val(extent_size);
 });
+
+function updateFields(){
+    const formData = new FormData(); 
+    
+    formData.append("field_name", $("#field_name").val());
+    
+    const location_x = parseInt($("#field_location_x").val());
+    formData.append("x", location_x);
+
+    const location_y = parseInt($("#field_location_y").val());
+    formData.append("y", location_y);
+
+    formData.append("extent_size", $("#field_size").val());
+
+    formData.append("field_image1", $("#field_image1")[0].files[0]);
+    formData.append("field_image2", $("#field_image2")[0].files[0]);
+
+    const fieldName = $("#field_name").val();
+    const url = `http://localhost:5050/green-shadow/api/v1/field/${fieldName}`;
+    $.ajax({
+        url: url,
+        method: "PATCH",
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function (result){
+            clearFields();
+            console.log(result);
+            alert("Field Update Successfull");
+            loadFields();
+        },
+        error: function (result){
+            clearFields();
+            console.log(result);
+            alert("Field Update Unsuccessfull");
+            loadFields();
+        }
+    });
+}
 
 function clearFields(){
     $("#field_name").val('');

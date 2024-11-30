d29f8e5addb3ab31b3d9e6786b7161738eea3a9c
glen654 <glenalloy@gmail.com>
2024-11-30 11:14:00 +0530

    implemented loadVehicle function vehicle controller

--- Green-Shadow-front-end/controllers/VehicleController.js
+++ Green-Shadow-front-end/controllers/VehicleController.js
@@ -1,3 +1,44 @@
+$(document).ready(function() {
+    loadVehicle();
+});
+
+function loadVehicle(){
+    $.ajax({
+        url: 'http://localhost:5050/green-shadow/api/v1/vehicle',
+        type: 'GET',           
+        contentType: 'application/json', 
+        success: function(vehicle) {
+            console.log("Vehicle loaded:", vehicle);
+            $("#vehicle-table").empty();
+            
+            vehicle.forEach(function(vehicle) {
+                var record = `
+                    <tr>
+                        <td class="vehicle-license-value">${vehicle.licensePlateNumber}</td>
+                        <td class="vehicle-category-value">${vehicle.vehicleCategory}</td>
+                        <td class="vehicle-fuel-value">${vehicle.fuelType}</td>
+                        <td class="vehicle-status-value">${vehicle.status}</td>
+                        <td class="vehicle-remarks-value">${vehicle.remarks}</td>
+                        <td>
+                            <button class="btn btn-primary btn-sm update-button">
+                                <i class="fa fa-pencil"></i>
+                            </button>
+                            <button class="btn btn-danger btn-sm delete-button">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>`;
+                $("#vehicle-table").append(record);
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error("Failed to load vehicle:", error);
+            alert("An error occurred while loading the vehicle data.");
+        }
+    });
+}
+
+
 function saveVehicle(){
     var license_plate_number = $("#license_plate").val();
     var vehicle_category = $("#category").val();

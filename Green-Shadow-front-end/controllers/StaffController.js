36e8ea9ce272650025ed7c6a7d85e2b8bd37f768
glen654 <glenalloy@gmail.com>
2024-11-30 10:57:53 +0530

    implemented loadstaff function in staff controller

--- Green-Shadow-front-end/controllers/StaffController.js
+++ Green-Shadow-front-end/controllers/StaffController.js
@@ -1,3 +1,47 @@
+$(document).ready(function() {
+    loadStaff();
+});
+
+function loadStaff(){
+    $.ajax({
+        url: 'http://localhost:5050/green-shadow/api/v1/staff',
+        type: 'GET',           
+        contentType: 'application/json', 
+        success: function(staff) {
+            console.log("Staff loaded:", staff);
+            $("#staff-table").empty();
+            
+            staff.forEach(function(staff) {
+                const fullName = staff.first_name + " " + staff.last_name;
+                var record = `
+                    <tr>
+                        <td class="staff-name-value">${fullName}</td>
+                        <td class="staff-designation-value">${staff.designation}</td>
+                        <td class="staff-gender-value">${staff.gender}</td>
+                        <td class="staff-joinedDate-value">${staff.joined_date}</td>
+                        <td class="staff-dob-value">${staff.dob}</td>
+                        <td class="staff-contact-value">${staff.contact_no}</td>
+                        <td class="staff-email-value">${staff.email}</td>
+                        <td class="staff-role-value">${staff.role}</td>
+                        <td>
+                            <button class="btn btn-primary btn-sm update-button">
+                                <i class="fa fa-pencil"></i>
+                            </button>
+                            <button class="btn btn-danger btn-sm delete-button">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>`;
+                $("#staff-table").append(record);
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error("Failed to load staff:", error);
+            alert("An error occurred while loading the staff data.");
+        }
+    });
+}
+
 function saveStaff(){
     var first_name = $("#first_name").val();
     var last_name = $("#last_name").val();
@@ -10,11 +54,8 @@ function saveStaff(){
     var email = $("#email").val();
     var role = $("#role").val();
     var field_name = $("#staff_field_details").val();
-    var vehicle_name = $("#vehicle_name").val();
     
     const fields = field_name ? [{ field_name: field_name }] : [];
-
-    const vehicles = vehicle_name ? [{ vehicle_name: vehicle_name }] : [];
 
     $.ajax({
         url: " http://localhost:5050/green-shadow/api/v1/staff",
@@ -31,8 +72,7 @@ function saveStaff(){
             "contact_no": contact,
             "email": email,
             "role": role,
-            "fields": fields,
-            "vehicles": vehicles
+            "fields": fields
         }),
         success: function (result){
             clearStaffForm();
